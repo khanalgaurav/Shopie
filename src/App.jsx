45e36a6abd9aea0c8f34e58173626bddd6af0f04
khanalgaurav <khanalgaurav96@gmail.com>
2024-08-24 09:51:02 +0545

    Clear the Default vite app and Add the pages with Proper routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import ProductDetails from './pages/ProductDetails'
+import Cart from './pages/Cart'
+import Checkout from './pages/Checkout'
+import Account from './pages/Account'
+import Contact from './pages/Contact'
+import LoginSignup from './pages/LoginSignup'
+import Wishlist from './pages/Wishlist'
+import PageNotFound from './pages/PageNotFound'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/cart' element={<Cart/>}/>
+          <Route path='/checkout' element={<Checkout/>}/>
+          <Route path='/user/account' element={<Account/>}/>
+          <Route path='/contact' element={<Contact/>}/>
+          <Route path='/login' element={<LoginSignup/>}/>
+          <Route path='/product' element={<ProductDetails/>}>
+            <Route path=':productId' element={<ProductDetails/>}/>
+          </Route>
+          <Route path='/wishlist' element={<Wishlist/>}/>
+          <Route path='/*' element={<PageNotFound/>}/>         
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Offers from '../components/Offers'
+
+const Home = () => {
+  return (
+    <Offers/>
+  )
+}
+
+export default Home
