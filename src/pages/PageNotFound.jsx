ceaaf08fd079c021ba3f0605a0f7271d76168e4b
khanalgaurav <khanalgaurav96@gmail.com>
2024-09-13 18:03:50 +0545

    Build site

--- src/pages/PageNotFound.jsx
+++ src/pages/PageNotFound.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PageNotFound = () => {
-  return (
-    <div>PageNotFound</div>
-  )
+    return (
+        <div className="flex justify-center items-center my-10">
+            <div className="p-20">
+                <h1 className="text-7xl font-bold">404 Not Found</h1>
+                <p className="text-center py-5">
+                    The page you visited is not found. Please return to the home
+                    page
+                </p>
+                <div className="flex justify-center">
+                    <Link to="/">
+                        <button className="bg-orange text-white py-2 px-5 rounded-md">
+                            Back to Home page
+                        </button>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default PageNotFound
